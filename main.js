75ab0fad3d4ec5f91dfcf13a97659e20939fe5e3
kaitlyn978 <76715226+kaitlyn978@users.noreply.github.com>
2022-07-28 18:56:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition= new SpeechRecognition();
+
+function start(){
+    document.getElementById("microphone").src=""
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+
+}
+
+recognition.onresult=function(event){
+    console.log(event);
+var content=event.results[0][0].transcript;
+console.log(content);
+document.getElementById("textbox").innerHTML=content;
+if(content=="take my selfie"){
+    console.log("take my selfie--");
+    speak();
+}
+
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data="take selfie in 5 seconds";
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+},5000);
+}
+
+    
+    
+
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='image' src='"+data_uri+"'>";
+    });
+
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("image").src;
+    link.href=img;
+    link.click();
+}
